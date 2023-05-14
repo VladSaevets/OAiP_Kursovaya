d86f8416505746c406b0d60713787f211c29e7d7
VladSaevets <vladislavsaevets2015@gmail.com>
2023-05-14 14:53:43 +0300

    Добавлена сортировка расчёской

--- OAiP_Kursovaya/OAiP_Kursovaya.cpp
+++ OAiP_Kursovaya/OAiP_Kursovaya.cpp
@@ -15,7 +15,34 @@ int main()
         std::cin >> mass[i];    //ввод элементов массива с клавиатуры
     }
 
+    //введенный массив
     std::cout << "Введенный массив: ";
     for (int i = 0; i < mLength; i++) std::cout << mass[i] << " ";
     std::cout << "\n";
+
+    const double factor = 1.247; // Фактор уменьшения
+    int step = mLength - 1;
+    int temp;
+
+    //сортировка расчёской
+    while (step >= 1) {
+        for (int i = 0; i + step < mLength; ++i) {
+            if (mass[i] > mass[i + step]) {
+                temp = mass[i];
+                mass[i] = mass[i + step];
+                mass[i + step] = temp;
+            }
+        }
+        step /= factor;
+    }
+    //конечная сортировка пузырьком
+    for (size_t idx_i = 0; idx_i + 1 < mLength; ++idx_i) {
+        for (size_t idx_j = 0; idx_j + 1 < mLength - idx_i; ++idx_j) {
+            if (mass[idx_j + 1] < mass[idx_j]) {
+                temp = mass[idx_j];
+                mass[idx_j] = mass[idx_j + 1];
+                mass[idx_j + 1] = temp;
+            }
+        }
+    }
 }
